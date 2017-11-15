609539ae26713e08dd14a31b02c05ddde7aee971
Seth Fitzsimmons <seth@mojodna.net>
2017-11-14 20:19:25 -0800

    Azure EventHub sink

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const BinarySplitter = require("./binary_splitter");
+const EventHubSink = require("./sinks/event_hub");
 const KinesisSink = require("./sinks/kinesis_stream");
 const Changes = require("./sources/change_stream");
 const Changesets = require("./sources/changeset_stream");
@@ -7,6 +8,7 @@ const KinesisSource = require("./sources/kinesis_stream");
 module.exports = {
   BinarySplitter,
   sinks: {
+    EventHub: EventHubSink,
     Kinesis: KinesisSink
   },
   sources: {
---
+++ src/sinks/event_hub.js
@@ -0,0 +1,65 @@
+const { Writable } = require("stream");
+
+const async = require("async");
+const { Client } = require("azure-event-hubs");
+
+const CONCURRENCY = 128;
+
+class EventHub extends Writable {
+  constructor(connectionString, path = null) {
+    super();
+
+    this.client = Client.fromConnectionString(connectionString, path);
+    this.sender = null;
+    this.pending = 0;
+  }
+
+  async connect() {
+    if (this.sender != null) {
+      return this.sender;
+    }
+
+    await this.client.open();
+
+    this.sender = await this.client.createSender();
+
+    this.sender.on("errorReceived", err =>
+      console.warn("sender error:", err.stack)
+    );
+
+    return this.sender;
+  }
+
+  _write(chunk, encoding, callback) {
+    this.pending++;
+
+    let blind = false;
+
+    if (this.pending < CONCURRENCY) {
+      // report successful writes preemptively
+      blind = true;
+      callback();
+    }
+
+    return (
+      this.connect()
+        .then(sender => sender.send(chunk.toString()))
+        // callback if this wasn't submitted blindly
+        .then(() => blind || callback())
+        .catch(callback)
+        .then(() => this.pending--)
+    );
+  }
+
+  _final(callback) {
+    // wait until all pending writes have flushed
+    return async.until(() => this.pending === 0, setImmediate, () =>
+      this.client
+        .close()
+        .then(callback)
+        .catch(callback)
+    );
+  }
+}
+
+module.exports = EventHub;
