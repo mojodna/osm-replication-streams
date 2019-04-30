93fb1cf8878febd63527dae12d409ab121303936
Seth Fitzsimmons <seth@mojodna.net>
2019-04-30 15:17:31 -0700

    Drop support for Azure Event Hubs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const BinarySplitter = require("./binary_splitter");
 const AugmentedDiffParser = require("./parsers/adiff");
-const EventHubSink = require("./sinks/event_hub");
 const KinesisSink = require("./sinks/kinesis_stream");
 const AugmentedDiffs = require("./sources/adiff_stream");
 const Changes = require("./sources/change_stream");
 const Changesets = require("./sources/changeset_stream");
-const EventHubSource = require("./sources/event_hub");
 const KinesisSource = require("./sources/kinesis_stream");
 
 module.exports = {
@@ -14,14 +12,12 @@ module.exports = {
     AugmentedDiffParser,
   },
   sinks: {
-    EventHub: EventHubSink,
     Kinesis: KinesisSink
   },
   sources: {
     AugmentedDiffs,
     Changes,
     Changesets,
-    EventHub: EventHubSource,
     Kinesis: KinesisSource
   }
 }
